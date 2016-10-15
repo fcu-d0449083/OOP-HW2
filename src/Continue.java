b7b80e35b41068779041662ef966c9716f0408b1
fcu-d0449083 <hqwer08@gmail.com>
2016-10-15 18:08:28 +0800

    homework3

---
+++ src/Continue.java
@@ -0,0 +1,16 @@
+
+public class Continue {
+
+	public static void main(String[] args) {
+
+	
+		
+				for(int i = 0; i < 5; i ++) {
+					if(i%2==0)
+				continue;
+				System.out.println(i);
+
+	}
+
+}
+	}
